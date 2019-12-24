cc4a877a7b745f3e599733ef37b471af26120592
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-24 22:07:25 +0300

    adding youtube API component

---
+++ src/APIs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+const KEY = "AIzaSyBmUuebjjFKzsgKZ8nuaf90VN1Fsscs-Ek";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    type: "video",
+    key: `${KEY}`
+  }
+});
