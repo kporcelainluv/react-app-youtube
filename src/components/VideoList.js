0ce5be0cf5fae67af778107fcfb6f77888c07eab
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-25 17:08:06 +0300

    add callback to videoList component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { VideoItem } from "./VideoItem";
 
-export const VideoList = ({ videos }) => {
+export const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(v => {
-    return <VideoItem video={v} />;
+    return (
+      <VideoItem video={v} onVideoSelect={onVideoSelect} key={v.id.videoId} />
+    );
   });
   return <div className={"ui relaxed divided list"}>{renderedList}</div>;
 };
